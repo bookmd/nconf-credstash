fcf580c41e3e341a5c514c8002136b1769667e22
Meir Shalev <meirshal@gmail.com>
2017-02-19 09:58:46 +0200

    initial commit (#1)
    * initial commit
    
    * CR fixes
    
    * CR fixes
    
    * CR fixes
    
    * Documentation
    
    * Update README.md
    
    * Update README.md
    
    * Update package.json

---
+++ README.md
@@ -0,0 +1,48 @@
+# nconf-credstash
+A [CredStash](https://github.com/fugue/credstash) store for [nconf](https://github.com/indexzero/nconf).
+Enabling managing secrets in CredStash and integrating them with nconf.
+
+## Installation
+### Setup CredStash
+[Install and setup CredStash](https://github.com/fugue/credstash#setup).
+
+### Install nconf-credstash
+``` bash
+  $ npm install nconf
+  $ npm install nconf-credstash
+```
+## Usage
+### Adding the store
+To add `nconf-credstash` to the `nconf` hierarchy, just import `nconf-credstash` and use the `.use()` method, the following way:
+```javascript
+require('nconf-credstash');
+
+nconf.use('nconf-credstash', { prefix: 'credstash_' , separator: '.'});
+```
+### Fetching secrets
+The store provided by `nconf-credstash` supports two ways of fetching secrets:
+
+1. `.get(key)` to fetch a specific key. For every call to `.get(key)` the store searches it's inner cache and only if the key doen't exist goes to CredStash to fetch it.
+2. `.get()` to fetch the entire store from CredStash. The store is cached for subsequent calls.
+
+All calls are synchronous, using node's abillity to spawn child processes synchronously.
+
+### Providing a prefix
+A prefix can be used in order not to fetch unwanted keys, since each call to CredStash takes a few seconds.
+If the `nconf-credstash` is set up with a prefix, only keys that begins with the prefix will be fetched. If the key has a long path (like 'path:to:key'), it will only be fetched if the last part begins with the prefix. 
+For example, if the prefix is "credstash_", the keys "credstash_password" and "postgres:credstash_password" will be fetched and the keys "password", "postgres:password" or "credstash_postgres:password" will not be fetched. 
+
+The prefix should not be a part of the key in CredStash. "postgres:credstash_password" should be saved in CredStash as "postgres.password".
+
+### Other options
+1. `separator` - a separator string for keys in CredStash. Requierd.
+2. `table` - The table in DynamoDB. The default is credential-store. Optional.
+3. `region` - AWS region. Optional.
+4. `context` - [CredStash context object](https://github.com/fugue/credstash#controlling-and-auditing-secrets). Optional.
+
+## Running tests
+The tests are writen in Jasmine.
+``` bash
+  $ npm test
+```
+
---
+++ spec/lib/nconf-credstash.spec.js
@@ -0,0 +1,97 @@
+/**
+ * Created by meirshalev on 14/02/2017.
+ */
+
+var nconf;
+var credstash;
+
+var CredstashApi = require('../../lib/credstashApi');
+var credstashApi = new CredstashApi({ table: 'nconf-credstash-tests' });
+
+const values = ['my_mongo_password', 'my_postgres_password'];
+const keys = ['mongo.password', 'postgres.password'];
+const confFile = './spec/lib/config.json';
+
+describe('nconf-credstash', function() {
+  beforeAll(function() {
+    for (var i=0; i<keys.length; i++) {
+      credstashApi.put(keys[i], values[i]);
+    }
+  });
+
+  afterAll(function() {
+    keys.forEach(function(key) {
+      credstashApi.delete(key);
+    });
+  });
+
+  beforeEach(function() {
+    nconf = require('nconf');
+    credstash = require('../../index');
+    nconf.use('credstash', { prefix: 'credstash_', separator: '.' })
+    .file({ file: confFile });
+
+  });
+
+  it('returns the key postgres.credstash_password', function() {
+    var password = nconf.get('postgres:credstash_password');
+    expect(password).toEqual('my_postgres_password');
+  });
+
+  it('gets the key from the store and not from CredStash', function() {
+    var beginTime = new Date().getTime();
+    var password = nconf.get('postgres:credstash_password');
+    var endTime = new Date().getTime();
+
+    expect(password).toEqual('my_postgres_password');
+    expect(endTime-beginTime).toBeLessThan(10); // CredStash calls take seconds, so 10 milliseconds is small enough.
+  });
+
+  it('retrieves the entire configuration using nconf.get()', function() {
+    var conf = nconf.get();
+    var expected = {
+      'foo': 'bar',
+      'postgres': {
+        'url': 'i am postgres url',
+        'credstash_password': 'my_postgres_password'
+      },
+      'mongo': {
+        'url': 'i am mongo url',
+        'credstash_password': 'my_mongo_password'
+      }
+    };
+    expect(conf).toEqual(expected);
+  });
+
+  it('skips keys that don\'t start with the prefix', function() {
+    var beginTime = new Date().getTime();
+    var bar = nconf.get('foo');
+    var endTime = new Date().getTime();
+
+    expect(bar).toEqual('bar');
+    expect(endTime-beginTime).toBeLessThan(10); // CredStash calls take seconds, so 10 milliseconds is small enough.
+  });
+
+  it('merges the configurations according to order of the stores', function() {
+    nconf = new nconf.Provider(); // Recreate the nconf object, the same way it's being done on nconf.js
+    nconf.Credstash = credstash; // Add again Credstash to the nconf object.
+
+    nconf.file({ file: confFile })
+    .use('credstash', { prefix: 'credstash_', separator: '.' });
+
+    var expected = {
+      'foo': 'bar',
+      'postgres': {
+        'url': 'i am postgres url',
+        'credstash_password': ''
+      },
+      'mongo': {
+        'url': 'i am mongo url',
+        'credstash_password': 'my_mongo_password'
+      }
+    };
+    var conf = nconf.get();
+
+    expect(conf).toEqual(expected);
+  });
+});
