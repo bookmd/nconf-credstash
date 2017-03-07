aa4c08e26a796a1ad947e8c7186200c1c9bbbf21
Asaf David <asafdav@gmail.com>
2017-03-07 13:00:19 +0200

    Fix performance issues

--- spec/lib/nconf-credstash.spec.js
+++ spec/lib/nconf-credstash.spec.js
@@ -8,47 +8,41 @@ var credstash;
 var CredstashApi = require('../../lib/credstashApi');
 var credstashApi;
 
-const values = ['my_mongo_password', 'my_postgres_password'];
-const keys = ['mongo.password', 'postgres.password'];
+const secrets = {
+  mongo: {
+    password: 'my_mongo_password',
+  },
+  postgres: {
+    password: 'my_postgres_password'
+  }
+};
+
 const confFile = './spec/lib/config.json';
 const TABLE_NAME = 'nconf-credstash-tests';
+const TEST_KEY = 'testkey';
 
 describe('nconf-credstash', function() {
   beforeAll(function() {
-    credstashApi = new CredstashApi({ table: TABLE_NAME });
+    credstashApi = new CredstashApi({ table: TABLE_NAME});
     credstashApi.setup(); // create the test DDB table, if not created
-
-    for (var i=0; i<keys.length; i++) {
-      credstashApi.put(keys[i], values[i]);
-    }
+    credstashApi.put(TEST_KEY, JSON.stringify(secrets));
   });
 
   afterAll(function() {
-    keys.forEach(function(key) {
-      credstashApi.delete(key);
-    });
+    credstashApi.delete(TEST_KEY);
   });
 
   beforeAll(function() {
     nconf = require('nconf');
     credstash = require('../../index');
-    nconf.use('credstash', { prefix: 'credstash_', separator: '.', table: TABLE_NAME })
-    .file({ file: confFile });
-
+    nconf
+      .use('credstash', { key: TEST_KEY, table: TABLE_NAME })
+      .file('defaults', confFile);
   });
 
-  it('returns the key postgres.credstash_password', function() {
-    var password = nconf.get('postgres:credstash_password');
+  it('returns the key postgres.password', function() {
+    var password = nconf.get('postgres.password');
     expect(password).toEqual('my_postgres_password');
-  });
-
-  it('gets the key from the store and not from CredStash', function() {
-    var beginTime = new Date().getTime();
-    var password = nconf.get('postgres:credstash_password');
-    var endTime = new Date().getTime();
-
-    expect(password).toEqual('my_postgres_password');
-    expect(endTime-beginTime).toBeLessThan(10); // CredStash calls take seconds, so 10 milliseconds is small enough.
   });
 
   it('retrieves the entire configuration using nconf.get()', function() {
@@ -57,23 +51,14 @@ describe('nconf-credstash', function() {
       'foo': 'bar',
       'postgres': {
         'url': 'i am postgres url',
-        'credstash_password': 'my_postgres_password'
+        'password': 'my_postgres_password'
       },
       'mongo': {
         'url': 'i am mongo url',
-        'credstash_password': 'my_mongo_password'
+        'password': 'my_mongo_password'
       }
     };
     expect(conf).toEqual(expected);
-  });
-
-  it('skips keys that don\'t start with the prefix', function() {
-    var beginTime = new Date().getTime();
-    var bar = nconf.get('foo');
-    var endTime = new Date().getTime();
-
-    expect(bar).toEqual('bar');
-    expect(endTime-beginTime).toBeLessThan(10); // CredStash calls take seconds, so 10 milliseconds is small enough.
   });
 
   it('merges the configurations according to order of the stores', function() {
@@ -81,17 +66,17 @@ describe('nconf-credstash', function() {
     nconf.Credstash = credstash; // Add again Credstash to the nconf object.
 
     nconf.file({ file: confFile })
-    .use('credstash', { prefix: 'credstash_', separator: '.', table: TABLE_NAME });
+    .use('credstash', { key: TEST_KEY, table: TABLE_NAME });
 
     var expected = {
       'foo': 'bar',
       'postgres': {
         'url': 'i am postgres url',
-        'credstash_password': ''
+        'password': 'CREDSTASH'
       },
       'mongo': {
         'url': 'i am mongo url',
-        'credstash_password': 'my_mongo_password'
+        'password': 'my_mongo_password'
       }
     };
     var conf = nconf.get();
