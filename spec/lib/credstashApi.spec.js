870d2a0afba03654f995fbd6e939c48cc048f8eb
Noah Guld <nguld12@gmail.com>
2017-05-10 14:05:06 +0300

    Update tests to check remote aswell.

--- spec/lib/credstashApi.spec.js
+++ spec/lib/credstashApi.spec.js
@@ -13,52 +13,62 @@ describe('CredstashApi', function() {
     });
   }
 
-  var api;
+  var api = [];
+  beforeEach(function() {
+    api.push(new CredstashApi({ table: TABLE_NAME }));
+    api.push(new CredstashApi({ table: TABLE_NAME, allowRemote: true }));
 
-  beforeAll(function() {
-    api = new CredstashApi({ table: TABLE_NAME });
-    api.setup();
+    api.forEach(function(credstashApi) {
+      credstashApi.setup();
+    });
   });
 
-  afterAll(function() {
-    cleanup(api);
+  afterEach(function() {
+    api.forEach(function(credstashApi) {
+      cleanup(credstashApi);
+    });
+    api = [];
   });
 
   describe('put, get and delete', function() {
-    it('puts a value into CredStash, gets it and deletes it', function() {
-      var value = 'value';
-      var key = 'key';
+    api.forEach(function(credstashApi) {
+      it('puts a value into CredStash, gets it and deletes it', function() {
+        var value = 'value';
+        var key = 'key';
 
-      api.put(key, value);
-      var returned = api.get(key);
-      expect(returned).toEqual(value);
-      api.delete(key);
+        credstashApi.put(key, value);
+        var returned = credstashApi.get(key);
+        expect(returned).toEqual(value);
+        credstashApi.delete(key);
 
-      var shouldNotBeUndefined = api.get(key);
-      expect(shouldNotBeUndefined).not.toBeDefined();
+        var shouldNotBeUndefined = credstashApi.get(key);
+        expect(shouldNotBeUndefined).not.toBeDefined();
+      });
     });
   });
 
   describe('put, getAll and delete', function() {
-    it('puts 2 values into CredStash, gets them and deletes them', function() {
-      var expected = {
-        key1: 'value1',
-        key2: 'value2'
-      };
+    api.forEach(function(credstashApi) {
+      it('puts 2 values into CredStash, gets them and deletes them', function() {
+        var expected = {
+          key1: 'value1',
+          key2: 'value2'
+        };
 
-      Object.keys(expected).forEach(function(key) {
-        api.put(key, expected[key]);
+        Object.keys(expected).forEach(function(key) {
+          credstashApi.put(key, expected[key]);
+        });
+
+        var returned = credstashApi.getAll();
+        expect(returned).toEqual(expected);
+
+        Object.keys(expected).forEach(function(key) {
+          credstashApi.delete(key);
+        });
+
+        var shouldBeEmpty = credstashApi.getAll();
+        expect(shouldBeEmpty).toEqual({});
       });
-
-      var returned = api.getAll();
-      expect(returned).toEqual(expected);
-
-      Object.keys(expected).forEach(function(key) {
-        api.delete(key);
-      });
-
-      var shouldBeEmpty = api.getAll();
-      expect(shouldBeEmpty).toEqual({});
     });
   });
 
@@ -68,53 +78,87 @@ describe('CredstashApi', function() {
     const FIRST_TABLE_KEY = 'only_on_first_table';
     const SECOND_TABLE_KEY = 'only_on_second_table';
 
-    afterAll(function() {
-      cleanup(api1);
-      cleanup(api2);
+    afterEach(function() {
+      api.forEach(function(credstashApi) {
+        cleanup(credstashApi.api1);
+        cleanup(credstashApi.api2);
+      });
+      api = [];
     });
 
     it('puts the values in the table specified in the constructor', function() {
       api1 = new CredstashApi({ table: 'firstTable' });
-      api1.setup();
       api2 = new CredstashApi({ table: 'secondTable' });
-      api2.setup();
 
-      // put a value on the first table
-      api1.put(FIRST_TABLE_KEY, FIRST_TABLE_KEY);
-      var value1 = api1.get(FIRST_TABLE_KEY);
-      expect(value1).toEqual(FIRST_TABLE_KEY);
+      var apiLocal = {};
+      apiLocal.api1 = api1;
+      apiLocal.api1 = api2;
 
-      // assert that it doesn't appear on the second table
-      var value2 = api2.get(FIRST_TABLE_KEY);
-      expect(value2).not.toBeDefined();
+      api1 = new CredstashApi({ table: 'firstTable', allowRemote: true });
+      api2 = new CredstashApi({ table: 'secondTable', allowRemote: true });
 
-      // put a value on the second table
-      api2.put(SECOND_TABLE_KEY, SECOND_TABLE_KEY);
-      var value3 = api2.get(SECOND_TABLE_KEY);
-      expect(value3).toEqual(SECOND_TABLE_KEY);
+      var apiRemote = {};
+      apiLocal.api1 = api1;
+      apiLocal.api1 = api2;
 
-      // assert that it doesn't appear on the first table
-      var value4 = api1.get(SECOND_TABLE_KEY);
-      expect(value4).not.toBeDefined();
+      api.push(apiLocal);
+      api.push(apiRemote);
+
+      api.forEach(function(credstash){
+        // set up credstash
+        credstash.api1.setup();
+        credstash.api2.setup();
+
+        // put a value on the first table
+        credstash.api1.put(FIRST_TABLE_KEY, FIRST_TABLE_KEY);
+        var value1 = credstash.api1.get(FIRST_TABLE_KEY);
+        expect(value1).toEqual(FIRST_TABLE_KEY);
+
+        // assert that it doesn't appear on the second table
+        var value2 = credstash.api2.get(FIRST_TABLE_KEY);
+        expect(value2).not.toBeDefined();
+
+        // put a value on the second table
+        credstash.api2.put(SECOND_TABLE_KEY, SECOND_TABLE_KEY);
+        var value3 = credstash.api2.get(SECOND_TABLE_KEY);
+        expect(value3).toEqual(SECOND_TABLE_KEY);
+
+        // assert that it doesn't appear on the first table
+        var value4 = credstash.api1.get(SECOND_TABLE_KEY);
+        expect(value4).not.toBeDefined();
+      });
     });
 
     it('puts values with encryption context and only gets them when using \'get()\' with the same context', function() {
       api1 = new CredstashApi({ table: TABLE_NAME, context: { env: 'test1' } });
       api2 = new CredstashApi({ table: TABLE_NAME, context: { env: 'test2' } });
 
-      // put a value on the first table
-      api1.put(FIRST_TABLE_KEY, FIRST_TABLE_KEY);
-      var value1 = api1.get(FIRST_TABLE_KEY);
-      expect(value1).toEqual(FIRST_TABLE_KEY);
-      // assert that it doesn't appear on the second table
-      expect(function() { api2.get(FIRST_TABLE_KEY) }).toThrow();
+      var apiLocal = {};
+      apiLocal.api1 = api1;
+      apiLocal.api1 = api2;
 
-      // put a value on the second table
-      api2.put(SECOND_TABLE_KEY, SECOND_TABLE_KEY);
-      var value3 = api2.get(SECOND_TABLE_KEY);
-      expect(value3).toEqual(SECOND_TABLE_KEY);
-      // assert that it doesn't appear on the first table
-      expect(function() { api1.get(SECOND_TABLE_KEY) }).toThrow();
+      api1 = new CredstashApi({ table: TABLE_NAME, context: { env: 'test1', allowRemote: true } });
+      api2 = new CredstashApi({ table: TABLE_NAME, context: { env: 'test2', allowRemote: true } });
+
+      var apiRemote = {};
+      apiLocal.api1 = api1;
+      apiLocal.api1 = api2;
+
+      api.forEach(function(credstash){
+        // put a value on the first table
+        credstash.api1.put(FIRST_TABLE_KEY, FIRST_TABLE_KEY);
+        var value1 = credstash.api1.get(FIRST_TABLE_KEY);
+        expect(value1).toEqual(FIRST_TABLE_KEY);
+        // assert that it doesn't appear on the second table
+        expect(function() { credstash.api2.get(FIRST_TABLE_KEY) }).toThrow();
+
+        // put a value on the second table
+        credstash.api2.put(SECOND_TABLE_KEY, SECOND_TABLE_KEY);
+        var value3 = credstash.api2.get(SECOND_TABLE_KEY);
+        expect(value3).toEqual(SECOND_TABLE_KEY);
+        // assert that it doesn't appear on the first table
+        expect(function() { credstash.api1.get(SECOND_TABLE_KEY) }).toThrow();
+      });
     });
   });
 });
