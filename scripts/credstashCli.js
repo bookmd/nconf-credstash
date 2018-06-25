69751096c2bca2bf9b0455f12568ff2d83d8c3de
idanziv <idan@getvim.com>
2018-06-25 19:15:48 +0300

    - added check for whether actionFlags is an array - removed left over function 'update' - removed option for choosing AWS region in the CLI because of a problem with the CredstashApi constructor. - moved some logic in the CLI to a separate function - now using string templates

--- scripts/credstashCli.js
+++ scripts/credstashCli.js
@@ -1,5 +1,5 @@
 /**
- * Created by Idan on 6/14/18
+ * Created by Idan on 06/14/2018
  */
 
 const process = require('process');
@@ -7,31 +7,33 @@ const program = require('commander');
 const _ = require('lodash');
 const CredstashApi = require('../lib/credstashApi');
 
+function convertObjectJsonPropertyToObject(object, key) {
+    if (typeof object[key] !== 'function' && object[key].charAt(0) === '{') {
+        try {
+            object[key] = JSON.parse(object[key]);
+        } catch (e) {
+            console.error(`Error parsing JSON.
+            Error message: ${e.toString()}
+            Key: ${key}
+            Value: ${object[key]}`);
+            process.exit(1);
+        }
+    }
+}
+
 function getValue(table, appKey = undefined, key = undefined) {
     this.credStashApi = new CredstashApi({table: table});
     let storedValue = null;
 
     if (appKey === undefined) {
         storedValue = this.credStashApi.getAll();
-        Object.keys(storedValue).forEach(
-            function (keyName) {
-                if (typeof storedValue[keyName] !== 'function' && storedValue[keyName].charAt(0) === '{') {
-                    try {
-                        storedValue[keyName] = JSON.parse(storedValue[keyName]);
-                    } catch (e) {
-                        console.error("Error parsing JSON from CredStash. Error message: " + e.toString());
-                        console.error("Offending key: " + keyName);
-                        console.error("Value: " + storedValue[keyName]);
-                        process.exit(1);
-                    }
-                }
-            });
+        Object.keys(storedValue).forEach((key) => convertObjectJsonPropertyToObject(storedValue, key));
     } else {
         storedValue = JSON.parse(this.credStashApi.get(appKey));
     }
 
     if (key !== undefined) {
-        storedValue = _.get(storedValue, key, "Key " + key + " does not exist");
+        storedValue = _.get(storedValue, key, `Key ${key} does not exist`);
     }
 
     console.log(JSON.stringify(storedValue, null, 4));
@@ -42,7 +44,7 @@ function updateValue(table, appKey, key, value) {
         try {
             value = JSON.parse(value);
         } catch (e) {
-            console.error("The value you entered is not a valid JSON.");
+            console.error(`The value you entered is not a valid JSON, check the formatting.`);
             process.exit();
         }
     }
@@ -52,30 +54,27 @@ function updateValue(table, appKey, key, value) {
 
     let oldValue = _.get(storedValue, key, undefined);
     storedValue = _.set(storedValue, key, value);
-    this.credStashApi.put(appKey, "'" + JSON.stringify(storedValue) + "'");
+    this.credStashApi.put(appKey, `'${JSON.stringify(storedValue)}'`);
     storedValue = JSON.parse(this.credStashApi.get(appKey));
     let newValue = _.get(storedValue, key);
-    console.log(table + "." + appKey + "." + key + ":\n" +
-                "Old Value: " + JSON.stringify(oldValue, null, 4) + "\n" +
-                "New Value: " + JSON.stringify(newValue, null, 4));
+    console.log(`${table}.${appKey}.${key}:
+        Old Value: ${JSON.stringify(oldValue, null, 4)}
+        New Value: ${JSON.stringify(newValue, null, 4)}`);
 }
 
 program
 .command('get <table> <appKey> [key]')
     .description('Read the value of a key in CredStash')
-    .option('-r, --region <regionName>', 'Region name')
     .action((table, appKey, key) => getValue(table, appKey, key));
 
 program
     .command('getall <table>')
     .description('Read all the values of an appKey in CredStash')
-    .option('-r, --region <regionName>', 'Region name')
     .action((table) => getValue(table));
 
 program
 .command('put <table> <appKey> <key> <value>')
     .description('Puts the value of a key in CredStash')
-    .option('-r, --region <regionName>', 'Region name')
     .action((table, appKey, key, value) => updateValue(table, appKey, key, value));
 
 program.parse(process.argv);
