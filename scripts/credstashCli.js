a1bfb40c9a616c66f00d98b9844cdb3171e837cd
idanziv <idan@getvim.com>
2018-06-27 10:37:14 +0300

    - added tests for CredstashCli - added setup and delete commands to cCli - fixed -r argument in CredstashApi and added ability in cCli to choose the region a command is performed against - Added function in cCli that all cApi interactions go through for cleaner code - Added function in cCli that prints to console for cleaner code
    SAM-3621

--- scripts/credstashCli.js
+++ scripts/credstashCli.js
@@ -1,80 +1,142 @@
 /**
  * Created by Idan on 06/14/2018
  */
-
 const process = require('process');
 const program = require('commander');
 const _ = require('lodash');
 const CredstashApi = require('../lib/credstashApi');
 
-function convertObjectJsonPropertyToObject(object, key) {
+function _interactWithApi(credstashApiInstance, apiFunc, funcArgs) {
+    try {
+        if (funcArgs) {
+            if (!Array.isArray(funcArgs)) {
+                funcArgs = _.castArray(funcArgs);
+            }
+        } else {
+            funcArgs = [];
+        }
+        return apiFunc.apply(credstashApiInstance, funcArgs);
+    } catch (e) {
+        throw new Error(`There was an error working against CredstashApi. Error: ${e}`);
+    }
+}
+
+function _convertObjectJsonPropertyToObject(object, key) {
     if (typeof object[key] !== 'function' && object[key].charAt(0) === '{') {
         try {
             object[key] = JSON.parse(object[key]);
         } catch (e) {
-            console.error(`Error parsing JSON.
+            throw new Error(`Error parsing JSON.
             Error message: ${e.toString()}
             Key: ${key}
             Value: ${object[key]}`);
-            process.exit(1);
         }
     }
 }
 
-function getValue(table, appKey = undefined, key = undefined) {
-    this.credStashApi = new CredstashApi({table: table});
+function _printToConsoleWithJsonFormatting(object) {
+    try {
+        console.log(JSON.stringify(object, null, 4));
+    } catch (e) {
+        throw new Error(`There was a problem printing the output as JSON, here is the raw output:\n
+        ${object.toString()}`)
+    }
+}
+
+function getValue(region = undefined, table, appKey = undefined, key = undefined) {
+    this.credStashApi = new CredstashApi({table: table, region: region});
     let storedValue = null;
 
     if (appKey === undefined) {
-        storedValue = this.credStashApi.getAll();
-        Object.keys(storedValue).forEach((key) => convertObjectJsonPropertyToObject(storedValue, key));
+        storedValue = _interactWithApi(this.credStashApi, this.credStashApi.getAll);
+        Object.keys(storedValue).forEach((key) => _convertObjectJsonPropertyToObject(storedValue, key));
     } else {
-        storedValue = JSON.parse(this.credStashApi.get(appKey));
+        storedValue = _interactWithApi(this.credStashApi, this.credStashApi.get, appKey);
+        storedValue = (storedValue !== undefined) ? JSON.parse(storedValue) : storedValue;
     }
 
-    if (key !== undefined) {
+    if (storedValue !== undefined && key !== undefined) {
         storedValue = _.get(storedValue, key, `Key ${key} does not exist`);
     }
 
-    console.log(JSON.stringify(storedValue, null, 4));
+    return storedValue;
 }
 
-function updateValue(table, appKey, key, value) {
+function putValue(region = undefined, table, appKey, key, value) {
     if (value.charAt(0) === '{') {
         try {
             value = JSON.parse(value);
         } catch (e) {
-            console.error(`The value you entered is not a valid JSON, check the formatting.`);
-            process.exit();
+            throw new Error(`The value you entered is not a valid JSON, check the formatting.`);
         }
     }
 
-    this.credStashApi = new CredstashApi({table: table});
-    let storedValue = JSON.parse(this.credStashApi.get(appKey));
+    this.credStashApi = new CredstashApi({table: table, region: region});
+    let storedValue = _interactWithApi(this.credStashApi, this.credStashApi.get, appKey);
+    storedValue = (storedValue === undefined) ? {} : JSON.parse(storedValue);
 
     let oldValue = _.get(storedValue, key, undefined);
     storedValue = _.set(storedValue, key, value);
-    this.credStashApi.put(appKey, `'${JSON.stringify(storedValue)}'`);
-    storedValue = JSON.parse(this.credStashApi.get(appKey));
-    let newValue = _.get(storedValue, key);
-    console.log(`${table}.${appKey}.${key}:
-        Old Value: ${JSON.stringify(oldValue, null, 4)}
-        New Value: ${JSON.stringify(newValue, null, 4)}`);
+    _interactWithApi(this.credStashApi, this.credStashApi.put, [appKey, JSON.stringify(storedValue)]);
+    storedValue = JSON.parse(_interactWithApi(this.credStashApi, this.credStashApi.get, appKey));
+
+    return {
+        path: `${table}.${appKey}.${key}:`,
+        oldValue: oldValue,
+        newValue: _.get(storedValue, key)
+    };
+}
+
+function deleteApp(region = undefined, table, appKey) {
+    this.credStashApi = new CredstashApi({table: table, region: region});
+    this.credStashApi.delete(appKey);
+}
+
+function setupTable(region = undefined, table) {
+    this.credStashApi = new CredstashApi({table: table, region: region});
+    this.credStashApi.setup();
 }
 
 program
-.command('get <table> <appKey> [key]')
+    .command('setup <table>')
+    .description('Creates a table in Credstash')
+    .option("-r, --region <region>", "The AWS region to work against")
+    .action(function(table, cmd) {
+        _printToConsoleWithJsonFormatting(setupTable(cmd.region, table));
+    });
+
+program
+    .command('get <table> <appKey> [key]')
     .description('Read the value of a key in CredStash')
-    .action((table, appKey, key) => getValue(table, appKey, key));
+    .option("-r, --region <region>", "The AWS region to work against")
+    .action(function (table, appKey, key, cmd) {
+        _printToConsoleWithJsonFormatting(getValue(cmd.region, table, appKey, key));
+    });
 
 program
     .command('getall <table>')
     .description('Read all the values of an appKey in CredStash')
-    .action((table) => getValue(table));
+    .option("-r, --region <region>", "The AWS region to work against")
+    .action(function (table, cmd) {
+        _printToConsoleWithJsonFormatting(getValue(cmd.region, table));
+    });
 
 program
-.command('put <table> <appKey> <key> <value>')
+    .command('put <table> <appKey> <key> <value>')
     .description('Puts the value of a key in CredStash')
-    .action((table, appKey, key, value) => updateValue(table, appKey, key, value));
+    .option("-r, --region <region>", "The AWS region to work against")
+    .action(function(table, appKey, key, value, cmd) {
+        _printToConsoleWithJsonFormatting(putValue(cmd.region, table, appKey, key, value));
+    });
+
+program
+    .command('delete <table> <appKey>')
+    .description('Delete an app from CredStash')
+    .option("-r, --region <region>", "The AWS region to work against")
+    .action(function (table, appKey, cmd) {
+        _printToConsoleWithJsonFormatting(deleteApp(cmd.region, table, appKey));
+    });
 
 program.parse(process.argv);
+
+module.exports = {getValue, putValue, deleteApp, setupTable};
