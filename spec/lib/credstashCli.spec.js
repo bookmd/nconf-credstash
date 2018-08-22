89a1d6f52e2f1ebf93de0139834838d41a963f36
idanziv <idan@getvim.com>
2018-08-22 18:21:47 +0300

    - fixed spacing - all function params for commands are now sent as one object - removed names from files - added "yarn credstash" task

--- spec/lib/credstashCli.spec.js
+++ spec/lib/credstashCli.spec.js
@@ -3,78 +3,79 @@
 const CredstashCli = require('../../scripts/credstashCli');
 const TABLE_NAME = 'nconf-credstash-tests';
 
-// the empty object is instead of the commander Command object
-describe('CredstashCli', function() {
-    function cleanup() {
-        let store = CredstashCli.getValue({}, TABLE_NAME);
-        Object.keys(store).forEach(function(key) {
-            CredstashCli.deleteApp({}, TABLE_NAME, key);
-        });
-    }
-
-    beforeAll(function() {
-        CredstashCli.setupTable({}, TABLE_NAME);
+describe('CredstashCli', function () {
+  function cleanup() {
+    let store = CredstashCli.getValue({table: TABLE_NAME});
+    Object.keys(store).forEach(function (key) {
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
     });
+  }
 
-    afterAll(function() {
-        cleanup();
+  beforeAll(function () {
+    CredstashCli.setupTable({table: TABLE_NAME});
+  });
+
+  afterAll(function () {
+    cleanup();
+  });
+
+  describe('putValue, getValue and delete', function () {
+    it('puts a value into CredStash, gets it and deletes it', function () {
+      let value = 'value';
+      let key = 'key';
+
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value});
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned).toEqual(value);
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+
+      let shouldNotBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(shouldNotBeUndefined).not.toBeDefined();
     });
+  });
 
-    describe('putValue, getValue and delete', function() {
-        it('puts a value into CredStash, gets it and deletes it', function() {
-            let value = 'value';
-            let key = 'key';
+  describe('putValue, getValue and delete', function () {
+    it('puts a value into CredStash, gets it, updates it, gets it and deletes it', function () {
+      let value = 'initialValue';
+      let value2 = 'updatedValue';
+      let key = 'key';
 
-            CredstashCli.putValue({}, TABLE_NAME, key, key, value);
-            let returned = CredstashCli.getValue({}, TABLE_NAME, key, key);
-            expect(returned).toEqual(value);
-            CredstashCli.deleteApp({}, TABLE_NAME, key);
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value});
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned).toEqual(value);
 
-            let shouldNotBeUndefined = CredstashCli.getValue({}, TABLE_NAME, key, key);
-            expect(shouldNotBeUndefined).not.toBeDefined();
-        });
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value: value2});
+      let returned2 = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned2).toEqual(value2);
+
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+      let shouldNotBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(shouldNotBeUndefined).not.toBeDefined();
     });
+  });
 
-    describe('putValue, getValue and delete', function() {
-        it('puts a value into CredStash, gets it, updates it, gets it and deletes it', function() {
-            let value = 'initialValue';
-            let value2 = 'updatedValue';
-            let key = 'key';
+  describe('put, get all and delete', function () {
+    it('puts 2 values into CredStash, gets them and deletes them', function () {
+      let expected = {
+        key1: 'value1',
+        key2: 'value2'
+      };
+      let key = 'key';
 
-            let l = CredstashCli.putValue({}, TABLE_NAME, key, key, value);
-            let returned = CredstashCli.getValue({}, TABLE_NAME, key, key);
-            expect(returned).toEqual(value);
+      Object.keys(expected).forEach(function (jsonKey) {
+        CredstashCli.putValue(
+          {table: TABLE_NAME, appKey: key, key: jsonKey, value: expected[jsonKey]}
+        );
+      });
 
-            l = CredstashCli.putValue({}, TABLE_NAME, key, key, value2);
-            let returned2 = CredstashCli.getValue({}, TABLE_NAME, key, key);
-            expect(returned2).toEqual(value2);
 
-            CredstashCli.deleteApp({}, TABLE_NAME, key);
-            let shouldNotBeUndefined = CredstashCli.getValue({}, TABLE_NAME, key);
-            expect(shouldNotBeUndefined).not.toBeDefined();
-        });
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(returned).toEqual(expected);
+
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+
+      let shouldBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(shouldBeUndefined).not.toBeDefined();
     });
-
-    describe('put, get all and delete', function() {
-        it('puts 2 values into CredStash, gets them and deletes them', function() {
-            let expected = {
-                key1: 'value1',
-                key2: 'value2'
-            };
-            let key = 'key';
-
-            Object.keys(expected).forEach(function(jsonKey) {
-                CredstashCli.putValue({}, TABLE_NAME, key, jsonKey, expected[jsonKey]);
-            });
-
-
-            let returned = CredstashCli.getValue({}, TABLE_NAME, key);
-            expect(returned).toEqual(expected);
-
-            CredstashCli.deleteApp({}, TABLE_NAME, key);
-
-            let shouldBeUndefined  = CredstashCli.getValue({}, TABLE_NAME, key);
-            expect(shouldBeUndefined).not.toBeDefined();
-        });
-    });
+  });
 });
