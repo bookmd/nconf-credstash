9cf710d71a812846582303aa7bbe9427b4abe8c8
Idan Ziv <kazzuya@gmail.com>
2018-08-23 10:56:28 +0300

    SAM-4320 - added support for AWS profiles/roles (#8)

--- scripts/credstashCli.js
+++ scripts/credstashCli.js
@@ -1,143 +1,174 @@
 "use strict";
 
-/**
- * Created by Idan on 06/14/2018
- */
 const process = require('process');
 const program = require('commander');
 const _ = require('lodash');
 const CredstashApi = require('../lib/credstashApi');
 
+function _checkIfProfileSpecified(cmd) {
+  if (cmd.profile === undefined) {
+    throw new Error('-p <profile> is required!');
+  }
+}
+
 function _interactWithApi(credstashApiInstance, apiFunc, funcArgs) {
-    try {
-        if (funcArgs) {
-            if (!Array.isArray(funcArgs)) {
-                funcArgs = _.castArray(funcArgs);
-            }
-        } else {
-            funcArgs = [];
-        }
-        return apiFunc.apply(credstashApiInstance, funcArgs);
-    } catch (e) {
-        throw new Error(`There was an error working against CredstashApi. Error: ${e}`);
+  try {
+    if (funcArgs) {
+      if (!Array.isArray(funcArgs)) {
+        funcArgs = _.castArray(funcArgs);
+      }
+    } else {
+      funcArgs = [];
     }
+    return apiFunc.apply(credstashApiInstance, funcArgs);
+  } catch (e) {
+    throw new Error(`There was an error working against CredstashApi. Error: ${e}`);
+  }
 }
 
 function _convertObjectJsonPropertyToObject(object, key) {
-    if (typeof object[key] !== 'function' && object[key].charAt(0) === '{') {
-        try {
-            object[key] = JSON.parse(object[key]);
-        } catch (e) {
-            throw new Error(`Error parsing JSON.
-            Error message: ${e.toString()}
-            Key: ${key}
-            Value: ${object[key]}`);
-        }
+  if (typeof object[key] !== 'function' && object[key].charAt(0) === '{') {
+    try {
+      object[key] = JSON.parse(object[key]);
+    } catch (e) {
+      throw new Error(`Error parsing JSON.
+        Error message: ${e.toString()}
+        Key: ${key}
+        Value: ${object[key]}`);
     }
+  }
 }
 
 function _printToConsoleWithJsonFormatting(object) {
+  try {
+    console.log(JSON.stringify(object, null, 4));
+  } catch (e) {
+    throw new Error(`There was a problem printing the output as JSON, here is the raw output:\n
+    ${object.toString()}`)
+  }
+}
+
+function getValue(params) {
+  const credStashApi = new CredstashApi(
+    { table: params.table, profile: params.profile, region: params.region }
+  );
+  const appKey = params.appKey;
+  const key = params.key;
+
+  let storedValue = null;
+
+  if (appKey === undefined) {
+    storedValue = _interactWithApi(credStashApi, credStashApi.getAll);
+    Object.keys(storedValue).forEach((key) => _convertObjectJsonPropertyToObject(storedValue, key));
+  } else {
+    storedValue = _interactWithApi(credStashApi, credStashApi.get, appKey);
+    storedValue = (storedValue !== undefined) ? JSON.parse(storedValue) : storedValue;
+  }
+
+  if (storedValue !== undefined && key !== undefined) {
+    storedValue = _.get(storedValue, key, `Key ${key} does not exist`);
+  }
+
+  return storedValue;
+}
+
+function putValue(params) {
+  let value = params.value;
+  const { table, appKey, key } = params;
+  if (value.charAt(0) === '{') {
     try {
-        console.log(JSON.stringify(object, null, 4));
+      value = JSON.parse(value);
     } catch (e) {
-        throw new Error(`There was a problem printing the output as JSON, here is the raw output:\n
-        ${object.toString()}`)
+      throw new Error(`The value you entered is not a valid JSON, check the formatting.`);
     }
+  }
+
+  const credStashApi = new CredstashApi(
+    { table: params.table, profile: params.profile, region: params.region }
+  );
+  let storedValue = _interactWithApi(credStashApi, credStashApi.get, appKey);
+  storedValue = (storedValue === undefined) ? {} : JSON.parse(storedValue);
+
+  const oldValue = _.get(storedValue, key, undefined);
+  storedValue = _.set(storedValue, key, value);
+  _interactWithApi(credStashApi, credStashApi.put, [appKey, JSON.stringify(storedValue)]);
+  storedValue = JSON.parse(_interactWithApi(credStashApi, credStashApi.get, appKey));
+
+  return {
+    path: `${table}.${appKey}.${key}:`,
+    oldValue: oldValue,
+    newValue: _.get(storedValue, key)
+  };
 }
 
-function getValue(region = undefined, table, appKey = undefined, key = undefined) {
-    const credStashApi = new CredstashApi({table: table, region: region});
-    let storedValue = null;
-
-    if (appKey === undefined) {
-        storedValue = _interactWithApi(credStashApi, credStashApi.getAll);
-        Object.keys(storedValue).forEach((key) => _convertObjectJsonPropertyToObject(storedValue, key));
-    } else {
-        storedValue = _interactWithApi(credStashApi, credStashApi.get, appKey);
-        storedValue = (storedValue !== undefined) ? JSON.parse(storedValue) : storedValue;
-    }
-
-    if (storedValue !== undefined && key !== undefined) {
-        storedValue = _.get(storedValue, key, `Key ${key} does not exist`);
-    }
-
-    return storedValue;
+function deleteApp(params) {
+  const credStashApi = new CredstashApi(
+    { table: params.table, profile: params.profile, region: params.region }
+  );
+  credStashApi.delete(params.appKey);
 }
 
-function putValue(region = undefined, table, appKey, key, value) {
-    if (value.charAt(0) === '{') {
-        try {
-            value = JSON.parse(value);
-        } catch (e) {
-            throw new Error(`The value you entered is not a valid JSON, check the formatting.`);
-        }
-    }
-
-    const credStashApi = new CredstashApi({table: table, region: region});
-    let storedValue = _interactWithApi(credStashApi, credStashApi.get, appKey);
-    storedValue = (storedValue === undefined) ? {} : JSON.parse(storedValue);
-
-    const oldValue = _.get(storedValue, key, undefined);
-    storedValue = _.set(storedValue, key, value);
-    _interactWithApi(credStashApi, credStashApi.put, [appKey, JSON.stringify(storedValue)]);
-    storedValue = JSON.parse(_interactWithApi(credStashApi, credStashApi.get, appKey));
-
-    return {
-        path: `${table}.${appKey}.${key}:`,
-        oldValue: oldValue,
-        newValue: _.get(storedValue, key)
-    };
-}
-
-function deleteApp(region = undefined, table, appKey) {
-    const credStashApi = new CredstashApi({table: table, region: region});
-    credStashApi.delete(appKey);
-}
-
-function setupTable(region = undefined, table) {
-    const credStashApi = new CredstashApi({table: table, region: region});
-    credStashApi.setup();
+function setupTable(params) {
+  const credStashApi = new CredstashApi(
+    { table: params.table, profile: params.profile, region: params.region }
+  );
+  credStashApi.setup();
 }
 
 program
-    .command('setup <table>')
-    .description('Creates a table in Credstash')
-    .option("-r, --region <region>", "The AWS region to work against")
-    .action(function(table, cmd) {
-        _printToConsoleWithJsonFormatting(setupTable(cmd.region, table));
-    });
+  .command('setup <table>')
+  .description('Creates a table in Credstash')
+  .option("-r, --region <region>", "The AWS region to work against")
+  .option("-p, --profile <profile>", "The AWS profile to use")
+  .action(function(table, cmd) {
+    _checkIfProfileSpecified(cmd);
+    const params = { region: cmd.region, profile: cmd.profile, table };
+    _printToConsoleWithJsonFormatting(setupTable(params));
+  });
 
 program
-    .command('get <table> <appKey> [key]')
-    .description('Read the value of a key in CredStash')
-    .option("-r, --region <region>", "The AWS region to work against")
-    .action(function (table, appKey, key, cmd) {
-        _printToConsoleWithJsonFormatting(getValue(cmd.region, table, appKey, key));
-    });
+  .command('get <table> <appKey> [key]')
+  .description('Read the value of a key in CredStash')
+  .option("-r, --region <region>", "The AWS region to work against")
+  .option("-p, --profile <profile>", "The AWS profile to use")
+  .action(function (table, appKey, key, cmd) {
+    _checkIfProfileSpecified(cmd);
+    const params = { region: cmd.region, profile: cmd.profile, table, appKey, key };
+    _printToConsoleWithJsonFormatting(getValue(params));
+  });
 
 program
-    .command('getall <table>')
-    .description('Read all the values of an appKey in CredStash')
-    .option("-r, --region <region>", "The AWS region to work against")
-    .action(function (table, cmd) {
-        _printToConsoleWithJsonFormatting(getValue(cmd.region, table));
-    });
+  .command('getall <table>')
+  .description('Read all the values of an appKey in CredStash')
+  .option("-r, --region <region>", "The AWS region to work against")
+  .option("-p, --profile <profile>", "The AWS profile to use")
+  .action(function (table, cmd) {
+    _checkIfProfileSpecified(cmd);
+    const params = { region: cmd.region, profile: cmd.profile, table };
+    _printToConsoleWithJsonFormatting(getValue(params));
+  });
 
 program
-    .command('put <table> <appKey> <key> <value>')
-    .description('Puts the value of a key in CredStash')
-    .option("-r, --region <region>", "The AWS region to work against")
-    .action(function(table, appKey, key, value, cmd) {
-        _printToConsoleWithJsonFormatting(putValue(cmd.region, table, appKey, key, value));
-    });
+  .command('put <table> <appKey> <key> <value>')
+  .description('Puts the value of a key in CredStash')
+  .option("-r, --region <region>", "The AWS region to work against")
+  .option("-p, --profile <profile>", "The AWS profile to use")
+  .action(function(table, appKey, key, value, cmd) {
+    _checkIfProfileSpecified(cmd);
+    const params = { region: cmd.region, profile: cmd.profile, table, appKey, key, value };
+    _printToConsoleWithJsonFormatting(putValue(params));
+  });
 
 program
-    .command('delete <table> <appKey>')
-    .description('Delete an app from CredStash')
-    .option("-r, --region <region>", "The AWS region to work against")
-    .action(function (table, appKey, cmd) {
-        _printToConsoleWithJsonFormatting(deleteApp(cmd.region, table, appKey));
-    });
+  .command('delete <table> <appKey>')
+  .description('Delete an app from CredStash')
+  .option("-r, --region <region>", "The AWS region to work against")
+  .option("-p, --profile <profile>", "The AWS profile to use")
+  .action(function (table, appKey, cmd) {
+    _checkIfProfileSpecified(cmd);
+    const params = { region: cmd.region, profile: cmd.profile, table, appKey };
+    _printToConsoleWithJsonFormatting(deleteApp(params));
+  });
 
 program.parse(process.argv);
 
--- spec/lib/credstashCli.spec.js
+++ spec/lib/credstashCli.spec.js
@@ -1,82 +1,81 @@
 "use strict";
 
-/**
- * Created by idanziv on 06/26/2018.
- */
 const CredstashCli = require('../../scripts/credstashCli');
 const TABLE_NAME = 'nconf-credstash-tests';
 
-describe('CredstashCli', function() {
-    function cleanup() {
-        let store = CredstashCli.getValue(undefined, TABLE_NAME);
-        Object.keys(store).forEach(function(key) {
-            CredstashCli.deleteApp(undefined, TABLE_NAME, key);
-        });
-    }
-
-    beforeAll(function() {
-        CredstashCli.setupTable(undefined, TABLE_NAME);
+describe('CredstashCli', function () {
+  function cleanup() {
+    let store = CredstashCli.getValue({table: TABLE_NAME});
+    Object.keys(store).forEach(function (key) {
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
     });
+  }
 
-    afterAll(function() {
-        cleanup();
+  beforeAll(function () {
+    CredstashCli.setupTable({table: TABLE_NAME});
+  });
+
+  afterAll(function () {
+    cleanup();
+  });
+
+  describe('putValue, getValue and delete', function () {
+    it('puts a value into CredStash, gets it and deletes it', function () {
+      let value = 'value';
+      let key = 'key';
+
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value});
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned).toEqual(value);
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+
+      let shouldNotBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(shouldNotBeUndefined).not.toBeDefined();
     });
+  });
 
-    describe('putValue, getValue and delete', function() {
-        it('puts a value into CredStash, gets it and deletes it', function() {
-            let value = 'value';
-            let key = 'key';
+  describe('putValue, getValue and delete', function () {
+    it('puts a value into CredStash, gets it, updates it, gets it and deletes it', function () {
+      let value = 'initialValue';
+      let value2 = 'updatedValue';
+      let key = 'key';
 
-            CredstashCli.putValue(undefined, TABLE_NAME, key, key, value);
-            let returned = CredstashCli.getValue(undefined, TABLE_NAME, key, key);
-            expect(returned).toEqual(value);
-            CredstashCli.deleteApp(undefined, TABLE_NAME, key);
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value});
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned).toEqual(value);
 
-            let shouldNotBeUndefined = CredstashCli.getValue(undefined, TABLE_NAME, key, key);
-            expect(shouldNotBeUndefined).not.toBeDefined();
-        });
+      CredstashCli.putValue({table: TABLE_NAME, appKey: key, key, value: value2});
+      let returned2 = CredstashCli.getValue({table: TABLE_NAME, appKey: key, key});
+      expect(returned2).toEqual(value2);
+
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+      let shouldNotBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(shouldNotBeUndefined).not.toBeDefined();
     });
+  });
 
-    describe('putValue, getValue and delete', function() {
-        it('puts a value into CredStash, gets it, updates it, gets it and deletes it', function() {
-            let value = 'initialValue';
-            let value2 = 'updatedValue';
-            let key = 'key';
+  describe('put, get all and delete', function () {
+    it('puts 2 values into CredStash, gets them and deletes them', function () {
+      let expected = {
+        key1: 'value1',
+        key2: 'value2'
+      };
+      let key = 'key';
 
-            let l = CredstashCli.putValue(undefined, TABLE_NAME, key, key, value);
-            let returned = CredstashCli.getValue(undefined, TABLE_NAME, key, key);
-            expect(returned).toEqual(value);
+      Object.keys(expected).forEach(function (jsonKey) {
+        CredstashCli.putValue(
+          {table: TABLE_NAME, appKey: key, key: jsonKey, value: expected[jsonKey]}
+        );
+      });
 
-            l = CredstashCli.putValue(undefined, TABLE_NAME, key, key, value2);
-            let returned2 = CredstashCli.getValue(undefined, TABLE_NAME, key, key);
-            expect(returned2).toEqual(value2);
 
-            CredstashCli.deleteApp(undefined, TABLE_NAME, key);
-            let shouldNotBeUndefined = CredstashCli.getValue(undefined, TABLE_NAME, key);
-            expect(shouldNotBeUndefined).not.toBeDefined();
-        });
+      let returned = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(returned).toEqual(expected);
+
+      CredstashCli.deleteApp({table: TABLE_NAME, appKey: key});
+
+      let shouldBeUndefined = CredstashCli.getValue({table: TABLE_NAME, appKey: key});
+      expect(shouldBeUndefined).not.toBeDefined();
     });
-
-    describe('put, get all and delete', function() {
-        it('puts 2 values into CredStash, gets them and deletes them', function() {
-            let expected = {
-                key1: 'value1',
-                key2: 'value2'
-            };
-            let key = 'key';
-
-            Object.keys(expected).forEach(function(jsonKey) {
-                CredstashCli.putValue(undefined, TABLE_NAME, key, jsonKey, expected[jsonKey]);
-            });
-
-
-            let returned = CredstashCli.getValue(undefined, TABLE_NAME, key);
-            expect(returned).toEqual(expected);
-
-            CredstashCli.deleteApp(undefined, TABLE_NAME, key);
-
-            let shouldBeUndefined  = CredstashCli.getValue(undefined, TABLE_NAME, key);
-            expect(shouldBeUndefined).not.toBeDefined();
-        });
-    });
+  });
 });
